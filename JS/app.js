f6503d008ae11f0bb172322a1efd762e2c6fcb95
Haustin Kimbrough <haustin.kimbrough@hotmail.com>
2021-07-14 16:48:54 -0700

    Adding two more questions to JS

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,15 @@
 'use strict';
-//ask a few questions and use if statements or switch statements to reply with a unique alert
+//Get the user's name and print it on the website after the rest of the script
 let userName = prompt('Please enter your name');
 document.write("Hello, " + userName + ". Welcome!");
+console.log('The user\'s name is ' +userName);
+
+//Question 1*******
 
 let iBackgroundQ = prompt('Haustin currently lives in Arizona, did he grow up there?');
+console.log('The user answered ' +iBackgroundQ+ ' to question 1.');
 let backgroundQ = iBackgroundQ.toLowerCase();
 let numCorrect = 0;
-console.log(iBackgroundQ);
-console.log(backgroundQ);
-
 if(backgroundQ === 'yes' || backgroundQ === 'y'){
   alert('Nope! Kentucky is home, but Tennessee is a close second! Being an Army brat makes you a nomad if you didn\'t know!');
 }
@@ -22,10 +23,11 @@ else {
     alert('Oops, that wasn\'t an option!');
 }
 
+//Question 2*******
+
 let iEducationQ = prompt('Has Haustin ever attended college?');
+console.log('The user answered ' +iEducationQ+ ' to question 2.');
 let educationQ = iEducationQ.toLowerCase();
-console.log(iEducationQ);
-console.log(educationQ);
 
 if(educationQ === 'yes' || educationQ === 'y'){
   alert('Correct! Technically, three universities and one college! No degree yet, but he\'s still working on it!');
@@ -41,10 +43,11 @@ else {
 }
 
 
+//Question 3******
+
 let iJobQ = prompt('He\'s been an Army brat, but was Haustin ever in the military?');
+console.log('The user answered ' +iJobQ+ ' to question 3.');
 let jobQ = iJobQ.toLowerCase();
-console.log(iJobQ);
-console.log(jobQ);
 
 if(jobQ === 'yes' || jobQ === 'y'){
   alert('That\'s right! He served for about seven years in the Army!');
@@ -59,10 +62,12 @@ else {
     alert('Oops, that wasn\'t an option!');
 }
 
+
+//Question 4*******
+
 let iGoalsQ = prompt('Does Haustin want to be a software developer for the rest of his career?');
+console.log('The user answered ' +iGoalsQ+ ' to question 4.');
 let goalsQ = iGoalsQ.toLowerCase();
-console.log(iGoalsQ);
-console.log(goalsQ);
 
 if(goalsQ === 'yes' || goalsQ === 'y'){
   alert('Oh no! That\'s not right. He hopes to own his own his own business one day.');
@@ -77,10 +82,12 @@ else {
     alert('Oops, that wasn\'t an option!');
 }
 
+
+//Question 5*******
+
 let iFunQ = prompt('Does Haustin have any pets?');
+console.log('The user answered ' +iFunQ+ ' to question 5.');
 let funQ = iFunQ.toLowerCase();
-console.log(iFunQ);
-console.log(funQ);
 
 if(funQ === 'yes' || funQ === 'y'){
   alert('DING DING DING!! Awesome! Haustin is a proud father to Bob Ross and Lenny Kravitz, two of the most spoiled kitties on the planet. You could probably include his daughter, Winter Sofia, on the list too since she is basically a wild animal.');
@@ -94,17 +101,75 @@ else if(funQ === 'no'|| funQ === 'n'){
 else {
     alert('Oops, that wasn\'t an option!');
 }
-if(numCorrect == 0){
+
+
+//Question 6*******
+
+var iUserGuess = prompt('Let\'s play a game! Haustin was born in May of 1991, what date was he born on? You\'ve got four chances to get it right!');
+var userGuess = parseInt(iUserGuess);
+var guessCount = 0;
+for (guessCount; guessCount < 4; guessCount++){
+  console.log('The user answered '+userGuess+ ' on guess number ' + (guessCount +1) + '.');
+  var chances = 4 - guessCount;
+  if (userGuess < 23){
+    iUserGuess = prompt('Nope! Sorry, you\'re too low. You\'ve got ' +chances+ ' chances left!');
+    userGuess = parseInt(iUserGuess);
+  }
+  else if (userGuess > 23){
+    iUserGuess = prompt('Nope! Sorry, you\'re too high. You\'ve got ' +chances+ ' chances left!');
+    userGuess = parseInt(iUserGuess);
+  }
+  else if(userGuess === 23){
+    alert('You got it right! Great job! He was born on 23 May 1991!');
+    numCorrect++;
+    break;
+  }
+}
+if (guessCount === 4){
+  alert('Sorry! You\'ve run out of guesses! The correct answer was 23!');
+  console.log('The user ran out of attempts for question 6');
+}
+
+//Question 7
+const myGames = ['Runescape', 'Heroes of the Storm', 'Call of Duty', 'Halo', 'Pokemon'];
+var guessCount = 6;
+let correctGame = false;
+while (!correctGame && guessCount > 0){
+  var iUserGuess = prompt ('Now, a different game! Haustin has loved only a few video games over the years, what is one of them? You have ' +guessCount+ ' chances remaining!');
+  console.log('The user answered ' +iUserGuess+ '.');
+  var userGuess = iUserGuess.toLowerCase();
+  for (let i = 0; i < myGames.length; i++){
+    if(userGuess === myGames[i].toLowerCase()){
+      alert('Correct! ' +iUserGuess+ ' is one of his favorites! ' +myGames+ ' are all of the games he loves!');
+      correctGame = true;
+    }
+  }
+  if(correctGame){
+    numCorrect++;
+  }
+  else if (!correctGame && guessCount <= 6 && guessCount > 1){
+    alert('Nope! Try again!');
+  }
+  else if (!correctGame && guessCount === 1) {
+    alert('Sorry! You\'ve run out of guesses! Possible choices were ' +myGames+ '.');
+  }
+  guessCount--;
+}
+
+
+
+//Calculate total questions answered correctly and display a response based on how well they did.
+if(numCorrect === 0){
   alert('Yikes, ' +userName+ '. You got none right. Haustin is very disappointed.');
 }
-else if(numCorrect == 1 || numCorrect == 2){
+else if(numCorrect >= 1 && numCorrect <= 3){
   alert('Uh oh, you only got ' + numCorrect + ' out of 5 questions right, ' +userName+ '. You better study up on Haustin!');
 }
-else if(numCorrect == 3){
+else if(numCorrect === 4 || numCorrect === 5){
   alert('Not too bad, ' +userName+ '! You got ' +numCorrect+ ' out of 5 questions right. Enjoy some more information about Haustin!');
 }
 else{
-  alert('Wow! Great job, ' +userName+ '. You got ' +numCorrect+ ' out of 5 questions right! *cough* stalker *cough*');
+  alert('Wow! Great job, ' +userName+ '. You got ' +numCorrect+ ' out of 7 questions right! *cough* stalker *cough*');
 }
 
 
